0e0fe4a77006ba28ff69058aa108d5acc11dbcd4
Korjubzot <korjubzot@gmail.com>
2023-05-31 13:18:13 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 # Virtual Pet
 
-Virtual Pet is a JavaScript based game that works like a Tamagotchi. Feed your pet, walk it, care for it, love it <3.
+Virtual Pet is a JavaScript based game that works like Tamagotchi. Feed your pet, walk it, care for it, love it <3.
 
 ## Installation
 
-N/A
+Fork and clone the Virtual Pet repository to your local machine. Run npm install to download any dependencies.
+
+In Node REPL, run the following commands to spawn a new pet.
 
 ## Usage
 
-N/A
+Take good care of your pet! Walk it regularly, make sure it's fed, let it grow up and adopt new pets as well. Check up on it to find out how it's feeling before anything goes wrong!
+
+If your pet gets too hungry (hunger value exceeding 10) it will DIE.
+
+If your pet gets too old (age 30) it will DIE.
+
+### Commands
+
+growUp() ages your pet 1, adds 5 hunger, and reduces their fitness by 3.
+
+walk() improves fitness by 4 points, to a maximum of 10.
+
+feed() reduces hunger by 3, to a minimum of 0.
+
+checkUp() tells you if your pet is hungry, restless, both, or none!
+
+adoptChild() lets your pet adopt a new baby. The circle of life really is wonderful.
 
 ## Contributing
 
--- src/pet.js
+++ src/pet.js
@@ -18,68 +18,70 @@ const maxHunger = 10;
 
 // Pet prototype
 function Pet(name) {
-    this.name = name;
-    this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
-    this.children = [];
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
+  this.children = [];
 }
 
 // Checks if pet is alive
 Pet.prototype = {
-    get isAlive() {
-      return this.age < maxAge && this.hunger < maxHunger && this.fitness > 0;
-    }
-  };
+  get isAlive() {
+    return this.age < maxAge && this.hunger < maxHunger && this.fitness > 0;
+  },
+};
 
-Pet.prototype.adoptChild = function(child) {
-    this.children.push(child);
-    return this.children;
-}
+Pet.prototype.adoptChild = function (child) {
+  this.children.push(child);
+  return this.children;
+};
 
-Pet.prototype.growUp = function() {
-    if (!this.isAlive) {
-        throw new Error ('Your pet is no longer alive :(')
-    } else {
+Pet.prototype.growUp = function () {
+  if (!this.isAlive) {
+    throw new Error("Your pet is no longer alive :(");
+  } else {
     this.age += 1;
     this.hunger += 5;
     this.fitness -= 3;
-    }
-    }
+  }
+};
 
-Pet.prototype.walk = function() {
-    if (!this.isAlive) {
-        throw new Error ('Your pet is no longer alive :(');
-    } else if ((this.fitness + fitnessIncrement) <= maxFitness ) {
-        this.fitness += fitnessIncrement;
-    } else {
-        this.fitness = maxFitness;
-    }
+Pet.prototype.walk = function () {
+  if (!this.isAlive) {
+    throw new Error("Your pet is no longer alive :(");
+  } else if (this.fitness + fitnessIncrement <= maxFitness) {
+    this.fitness += fitnessIncrement;
+  } else {
+    this.fitness = maxFitness;
+  }
+};
 
+Pet.prototype.feed = function () {
+  if (!this.isAlive) {
+    throw new Error("Your pet is no longer alive :(");
+  } else if (this.hunger - hungerIncrement >= minHunger) {
+    this.hunger -= hungerIncrement;
+  } else {
+    this.hunger = minHunger;
+  }
+};
 
-Pet.prototype.feed = function() {
-    if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive :(');
-    } else if ((this.hunger - hungerIncrement) >= minHunger ) {
-        this.hunger -= hungerIncrement;
-    } else {
-        this.hunger = minHunger;
-    }
-
-Pet.prototype.checkUp = function() {
-    if (!this.isAlive) {
-        return "Your pet is no longer alive :(";
-    } else if ((this.fitness <= fitnessThreshold) && (this.hunger >= hungerThreshold)) {
-        return "I am hungry AND I need a walk";
-    } else if (this.fitness <= fitnessThreshold) {
-        return "I need a walk";
-    } else if (this.hunger >= hungerThreshold) {
-        return "I am hungry";
-    } else {
-        return "I feel great!";
-    }
-}
-}
-}
+Pet.prototype.checkUp = function () {
+  if (!this.isAlive) {
+    return "Your pet is no longer alive :(";
+  } else if (
+    this.fitness <= fitnessThreshold &&
+    this.hunger >= hungerThreshold
+  ) {
+    return "I am hungry AND I need a walk";
+  } else if (this.fitness <= fitnessThreshold) {
+    return "I need a walk";
+  } else if (this.hunger >= hungerThreshold) {
+    return "I am hungry";
+  } else {
+    return "I feel great!";
+  }
+};
 
 module.exports = Pet;
